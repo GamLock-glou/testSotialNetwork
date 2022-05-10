556e1b326e9cf3cc8e4903f8874771037258e78a
GamLock-glou <klimenkov.z@mail.ru>
2022-05-10 12:52:38 +0300

    Updates

---
+++ precache-manifest.a9abf7aa181a046e859ccaa1460ccccc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e52e7b590f6b92276408a23198d55e",
+    "url": "/testSotialNetwork/index.html"
+  },
+  {
+    "revision": "16041355d4f1f96cd2fe",
+    "url": "/testSotialNetwork/static/css/3.aa8bf3fe.chunk.css"
+  },
+  {
+    "revision": "a2cd920d2142d693ed14",
+    "url": "/testSotialNetwork/static/css/4.b7b54eff.chunk.css"
+  },
+  {
+    "revision": "7fc1453d7a965062d8eb",
+    "url": "/testSotialNetwork/static/css/main.efc44217.chunk.css"
+  },
+  {
+    "revision": "45a28d0d15c04364d07b",
+    "url": "/testSotialNetwork/static/js/2.9636c4da.chunk.js"
+  },
+  {
+    "revision": "16041355d4f1f96cd2fe",
+    "url": "/testSotialNetwork/static/js/3.3dbd2a5d.chunk.js"
+  },
+  {
+    "revision": "a2cd920d2142d693ed14",
+    "url": "/testSotialNetwork/static/js/4.e87896ff.chunk.js"
+  },
+  {
+    "revision": "7fc1453d7a965062d8eb",
+    "url": "/testSotialNetwork/static/js/main.2ecd2eb6.chunk.js"
+  },
+  {
+    "revision": "c5ed9c0c05e096513a05",
+    "url": "/testSotialNetwork/static/js/runtime~main.fc8bba63.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/testSotialNetwork/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/testSotialNetwork/static/media/user.8b1dbb1f.png"
+  }
+]);
