65b8e6254c362aba6b39c470384dccbfc62e275c
GamLock-glou <klimenkov.z@mail.ru>
2022-05-10 14:47:44 +0300

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ const AppContainer = compose (
        )(App);
 
 export const MainApp = (props) => {
-       return <BrowserRouter>
+       return <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
